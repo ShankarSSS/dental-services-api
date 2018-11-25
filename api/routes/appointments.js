f598fa01ce9b61daa0b7e8c304dfcbd9b234e772
ShankarSSS <35001879+ShankarSSS@users.noreply.github.com>
2018-11-25 14:16:27 +0530

    Update appointments.js

--- api/routes/appointments.js
+++ api/routes/appointments.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+const Appointment = require('../models/appointment');
 
 routes.get('/', (req, res, next) => {
     res.status(200).json({
@@ -14,9 +15,32 @@ routes.get('/:appointmentId', (req, res, next) => {
     });
 });
 
-routes.post('/', (req, res, next) => {
+routes.post('/', (req, res, next) => {   
+
+    const appointment = new Appointment({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        mobile: req.body.mobile,
+        email: req.body.email,
+        source: req.body.source,
+        appointment_date: req.body.appointment_date,
+        appointment_start_date: req.body.appointment_start_date,
+        appointment_end_date: req.body.appointment_end_date,
+        created_date: req.body.created_date,
+        created_by: req.body.created_by,
+        product_id: req.body.product_id
+    });   
+
+
+    product.save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+
     res.status(200).json({
-        'Message': '/appointment handled in post'
+        'Message': '/appointment handled in post',
+        appointment: appointment
     });
 });
 
