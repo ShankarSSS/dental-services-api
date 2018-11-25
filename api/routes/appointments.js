e0ff4832e883ead7ea1153f5e56107b8a62b812b
ShankarSSS <35001879+ShankarSSS@users.noreply.github.com>
2018-11-25 20:47:20 +0530

    Update appointments.js

--- api/routes/appointments.js
+++ api/routes/appointments.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const Appointment = require('../models/appointment');
+const mongoose = require('mongoose');
+
 
 routes.get('/', (req, res, next) => {
     res.status(200).json({
@@ -16,6 +18,12 @@ routes.get('/:appointmentId', (req, res, next) => {
 });
 
 routes.post('/', (req, res, next) => {   
+try{
+    var appointment_date = new Date(req.body.appointment_date);
+    var appointment_start_date = new Date(req.body.appointment_date);
+    var appointment_end_date = new Date(req.body.appointment_date);
+    var created_date = new Date(req.body.appointment_date);
+
 
     const appointment = new Appointment({
         _id: new mongoose.Types.ObjectId(),
@@ -23,12 +31,11 @@ routes.post('/', (req, res, next) => {
         mobile: req.body.mobile,
         email: req.body.email,
         source: req.body.source,
-        appointment_date: req.body.appointment_date,
-        appointment_start_date: req.body.appointment_start_date,
-        appointment_end_date: req.body.appointment_end_date,
-        created_date: req.body.created_date,
-        created_by: req.body.created_by,
-        product_id: req.body.product_id
+        appointment_date:appointment_date,
+        appointment_start_date: appointment_start_date,
+        appointment_end_date:appointment_end_date,
+        created_date:created_date,
+        created_by: req.body.created_by       
     });   
 
 
@@ -39,9 +46,16 @@ routes.post('/', (req, res, next) => {
         .catch(err => console.log(err));
 
     res.status(200).json({
-        'Message': '/appointment handled in post',
+        'Message': '/appointment shankar handled in post',
         appointment: appointment
     });
+}
+catch(err){    
+    res.status(500).json({
+        'Message': err
+    });
+}
+    
 });
 
 routes.patch('/:appointmentId', (req, res, next) => {
